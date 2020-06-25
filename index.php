d35f45f6ad28eae7fdd6f361a2b1a550c82bccbb
黒川将志 <m.kurokawa.carecon@gmail.com>
2020-06-25 16:55:41 +0900

    初めての更新

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>gitサンプル</title>
+<link rel="stylesheet" type="text/css" href="../css/base.css">
+<link rel="stylesheet" type="text/css" href="../css/loginbase.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script>
+
+</script>
+</head>
+<body>
+<h1>Gitサンプル</h1>
+<p>操作test</p>
+</body>
+</html>
